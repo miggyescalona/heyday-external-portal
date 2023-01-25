eada941db41c82dc9ae3c89b6986d0bb60dd46a8
Miggy Escalona <miggyescalona@users.noreply.github.com>
2023-01-25 16:32:56 -0500

    update hardcoded urls

--- src/FileCabinet/SuiteScripts/cwgp/client/HEYDAY_CS_ListPage.js
+++ src/FileCabinet/SuiteScripts/cwgp/client/HEYDAY_CS_ListPage.js
@@ -11,14 +11,14 @@
  * @NScriptType ClientScript
  */
 
-define([], () => {
+define(['N/url', '../libraries/HEYDAY_LIB_ExternalPortal', '../libraries/HEYDAY_LIB_ClientExternalPortal'], (url, EPLib, ClientEPLib) => {
     /**
      * Function to be executed after page is initialized.
      *
      * @param {Object} context
      */
     const pageInit = (context) => {
-        getAuthenticationScript();
+        ClientEPLib.getAuthenticationScript();
     };
 
     /**
@@ -70,80 +70,40 @@ define([], () => {
 
     const toCreateTransaction = (stUserId, stAccessType, stType) => {
         //redurect to create transaction page
-        window.location = `https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=686&deploy=1&compid=5530036_SB1&h=b8a78be5c27a4d76e7a8&pageMode=create&userId=${stUserId}&accesstype=${stAccessType}&rectype=${stType}`;
+       const objCreateIntPOUrl = EPLib._CONFIG.RETAIL_PAGE[EPLib._CONFIG.ENVIRONMENT]
+        
+       let stCreateIntPOUrl = url.resolveScript({
+           deploymentId        : objCreateIntPOUrl.DEPLOY_ID,
+           scriptId            : objCreateIntPOUrl.SCRIPT_ID,
+           returnExternalUrl   : true,
+           params: {
+               pageMode    : 'create',
+               userId      : stUserId,
+               accesstype  : stAccessType,
+               rectype     : stType
+           }
+       });
+       window.location = stCreateIntPOUrl;
+   
+   
     };
 
     const back = (stUserId, stAccessType, stType) => {
-        window.location = `https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=682&deploy=1&compid=5530036_SB1&h=3eb96116ea1325a68f66&userId=${stUserId}&accesstype=${stAccessType}&rectype=${stType}`;
-    };
+        const objRenderUrl = EPLib._CONFIG.RENDER_PAGE[EPLib._CONFIG.ENVIRONMENT]
 
-    const getAuthenticationScript = () => {
-        const validateToken = async (token) => {
-            const result = await fetch('https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=683&deploy=1&compid=5530036_SB1&h=13bf4568597809ee949b', {
-                method: 'POST',
-                body: JSON.stringify({
-                    token: token,
-                    requestType: 'validateToken'
-                })
-            });
-
-            const objData = await result.json();
-
-            if (objData.message != 'success') {
-                return false;
+        let stRenderUrl = url.resolveScript({
+            deploymentId        : objRenderUrl.DEPLOY_ID,
+            scriptId            : objRenderUrl.SCRIPT_ID,
+            returnExternalUrl   : true,
+            params: {
+                userId      : stUserId,
+                accesstype  : stAccessType,
+                rectype     : stType
             }
-
-            return true;
-        };
-
-        const isLoggedIn = async () => {
-            const stToken = window.localStorage.getItem('token');
-
-            if (!stToken) { return false; }
-
-            const isValidToken = await validateToken(stToken);
-
-            if (isValidToken) {
-                window.localStorage.setItem('token', stToken);
-            }
-
-            return isValidToken;
-        };
-
-        isLoggedIn().then((result) => {
-            // If no token or not successful in validation, redirect to login page
-            if (!result) {
-                window.localStorage.removeItem('token');
-                window.location = 'https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=682&deploy=1&compid=5530036_SB1&h=3eb96116ea1325a68f66';
-
-                return;
-            }
-
-            const stToken = window.localStorage.getItem('token');
-       
-            if (stToken) {
-                const stDecodeToken = atob(stToken.split('.')[1]);
-                const { accessType } = JSON.parse(stDecodeToken);
-
-                const stQuery = window.location.search;
-                const objParams = new URLSearchParams(stQuery);
-                const stAccessTypeURL = objParams.get('accesstype');
-                console.log('stAccessTypeURL', stAccessTypeURL);
-
-                const bIsAccessTypeMismatched = (stAccessTypeURL != accessType);
-
-                if (bIsAccessTypeMismatched) {
-                    window.localStorage.removeItem('token');
-                    window.location = 'https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=682&deploy=1&compid=5530036_SB1&h=3eb96116ea1325a68f66';
-
-                    return;
-                }
-            }
-
-            const stBody = document.querySelector('body');
-            stBody.style.filter = 'none';
-            stBody.style.pointerEvents = 'auto';
         });
+
+        window.location = stRenderUrl;
+   
     };
 
     return {
--- src/FileCabinet/SuiteScripts/cwgp/client/HEYDAY_CS_ViewPage.js
+++ src/FileCabinet/SuiteScripts/cwgp/client/HEYDAY_CS_ViewPage.js
@@ -11,99 +11,90 @@
  * @NScriptType ClientScript
  */
 
-define([], () => {
+define(['N/url', '../libraries/HEYDAY_LIB_ExternalPortal', '../libraries/HEYDAY_LIB_ClientExternalPortal'], (url, EPLib, ClientEPLib) => {
 
     const pageInit = (context) => {
-        getAuthenticationScript();
+        ClientEPLib.getAuthenticationScript();
     };
 
     const toReceive = (stUserId, stPoId, stAccessType, stType) => {
-        //redirect to create transaction page
-        log.debug('toReceive', stUserId +'|' +stPoId + '|'+ stAccessType+'|'+stType)
-        window.location = `https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=686&deploy=1&compid=5530036_SB1&h=b8a78be5c27a4d76e7a8&pageMode=create&userId=${stUserId}&itemreceiptid=${stPoId}&accesstype=${stAccessType}&rectype=${stType}`;
+        
+        const objRetailUrl = EPLib._CONFIG.RETAIL_PAGE[EPLib._CONFIG.ENVIRONMENT]
+
+        let stReceiveRetailUrl = url.resolveScript({
+            deploymentId        : objRetailUrl.DEPLOY_ID,
+            scriptId            : objRetailUrl.SCRIPT_ID,
+            returnExternalUrl   : true,
+            params: {
+                pageMode    : 'edit',
+                userId      : stUserId,
+                itemreceiptid : stPoId,
+                accesstype  : stAccessType,
+                rectype     : stType //Retail PO or IR
+            }
+        })
+        window.location = stReceiveRetailUrl
+    
     };
 
     const toEdiTransaction = (stUserId, stPoId, stAccessType,stTranId,stType) => {
         log.debug('toEdiTransaction',stUserId +'|' +stPoId + '|'+ stAccessType+'|'+stType+'|'+stTranId);
-        if(stType == 'intercompanypo'){
-            window.location = `https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=686&deploy=1&compid=5530036_SB1&h=b8a78be5c27a4d76e7a8&pageMode=edit&userId=${stUserId}&poid=${stPoId}&accesstype=${stAccessType}&rectype=${stType}&tranid=${stTranId}`;
-        }
-        else if(stType == 'itemreceipt'){
-            window.location = `https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=686&deploy=1&compid=5530036_SB1&h=b8a78be5c27a4d76e7a8&pageMode=edit&userId=${stUserId}&itemreceiptid=${stPoId}&accesstype=${stAccessType}&rectype=${stType}&tranid=${stTranId}`;
-        }
+
+        const objRetailUrl = EPLib._CONFIG.RETAIL_PAGE[EPLib._CONFIG.ENVIRONMENT]
+        let stEditRetailUrl;
+
+        switch (stType){
+            case 'intercompanypo':
+                stEditRetailUrl = url.resolveScript({
+                    deploymentId        : objRetailUrl.DEPLOY_ID,
+                    scriptId            : objRetailUrl.SCRIPT_ID,
+                    returnExternalUrl   : true,
+                    params: {
+                        pageMode    : 'edit',
+                        userId      : stUserId,
+                        poid        : stPoId,
+                        accesstype  : stAccessType,
+                        rectype     : stType,
+                        tranid      : stTranId
+                    }
+                });
+                break;
+            case 'itemreceipt':
+                stEditRetailUrl = url.resolveScript({
+                    deploymentId        : objRetailUrl.DEPLOY_ID,
+                    scriptId            : objRetailUrl.SCRIPT_ID,
+                    returnExternalUrl   : true,
+                    params: {
+                        pageMode    : 'edit',
+                        userId      : stUserId,
+                        itemreceiptid : stPoId,
+                        accesstype  : stAccessType,
+                        rectype     : stType,
+                        tranid      : stTranId
+                    }
+                });
+        };
+        window.location = stEditRetailUrl
     };
 
     const back = (stUserId, stAccessType, stType) => {
-        window.location = `https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=686&deploy=1&compid=5530036_SB1&h=b8a78be5c27a4d76e7a8&pageMode=list&userId=${stUserId}&accesstype=${stAccessType}&rectype=${stType}`;
-    };
+          
+        const objRetailUrl = EPLib._CONFIG.RETAIL_PAGE[EPLib._CONFIG.ENVIRONMENT]
 
-    const getAuthenticationScript = () => {
-        const validateToken = async (token) => {
-            const result = await fetch('https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=683&deploy=1&compid=5530036_SB1&h=13bf4568597809ee949b', {
-                method: 'POST',
-                body: JSON.stringify({
-                    token: token,
-                    requestType: 'validateToken'
-                })
-            });
-
-            const objData = await result.json();
-
-            if (objData.message != 'success') {
-                return false;
+        let stRetailUrl = url.resolveScript({
+            deploymentId        : objRetailUrl.DEPLOY_ID,
+            scriptId            : objRetailUrl.SCRIPT_ID,
+            returnExternalUrl   : true,
+            params: {
+                pageMode    : 'list',
+                userId      : stUserId,
+                accesstype  : stAccessType,
+                rectype     : stType
             }
-
-            return true;
-        };
-
-        const isLoggedIn = async () => {
-            const stToken = window.localStorage.getItem('token');
-
-            if (!stToken) { return false; }
-
-            const isValidToken = await validateToken(stToken);
-
-            if (isValidToken) {
-                window.localStorage.setItem('token', stToken);
-            }
-
-            return isValidToken;
-        };
-
-        isLoggedIn().then((result) => {
-            // If no token or not successful in validation, redirect to login page
-            if (!result) {
-                window.localStorage.removeItem('token');
-                window.location = 'https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=682&deploy=1&compid=5530036_SB1&h=3eb96116ea1325a68f66';
-
-                return;
-            }
-
-            const stToken = window.localStorage.getItem('token');
-
-            if (stToken) {
-                const stDecodeToken = atob(stToken.split('.')[1]);
-                const { accessType } = JSON.parse(stDecodeToken);
-
-                const stQuery = window.location.search;
-                const objParams = new URLSearchParams(stQuery);
-                const stAccessTypeURL = objParams.get('accesstype');
-                console.log('stAccessTypeURL', stAccessTypeURL);
-
-                const bIsAccessTypeMismatched = (stAccessTypeURL != accessType);
-
-                if (bIsAccessTypeMismatched) {
-                    window.localStorage.removeItem('token');
-                    window.location = 'https://5530036-sb1.extforms.netsuite.com/app/site/hosting/scriptlet.nl?script=682&deploy=1&compid=5530036_SB1&h=3eb96116ea1325a68f66';
-
-                    return;
-                }
-            }
-
-            const stBody = document.querySelector('body');
-            stBody.style.filter = 'none';
-            stBody.style.pointerEvents = 'auto';
         });
+
+        window.location = stRetailUrl;
+    
     };
 
     return {
